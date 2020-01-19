92b7587137918a8688a130fd6718c26ccc25a967
ZheniaB <Venfurge@users.noreply.github.com>
2020-01-19 16:46:42 +0200

    First Commit
    Added ToolBar

---
+++ Xamarin_Forms_Task_2/Xamarin_Forms_Task_2/Xamarin_Forms_Task_2/View/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin_Forms_Task_2
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
---
+++ Xamarin_Forms_Task_2/Xamarin_Forms_Task_2/Xamarin_Forms_Task_2/ViewModel/MainPageVM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin_Forms_Task_2.ViewModel
+{
+    class MainPageVM
+    {
+    }
+}
